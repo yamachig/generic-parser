29464c00e341794a85cca98ab22572cd860783e9
yamachi <yamachig@users.noreply.github.com>
2021-04-15 18:40:29 -0700

    edit exports

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 export { RuleFactory } from "./rules/factory";
-export { Rule, BaseEnv, BasePos, UnknownRule, UnknownTarget, Empty, arrayLikeOffsetToPos, stringOffsetToPos, StringPos } from "./core";
+export { Rule, BaseEnv, BasePos, UnknownRule, UnknownTarget, Empty, arrayLikeOffsetToPos, stringOffsetToPos, StringPos, ValueOfRule } from "./core";
 export { parse as parsePegjs, defaultOptions as defaultParsePegjsOptions } from "./pegjs/pegjsParser";
 export { grammarToCode } from "./pegjs/astToCode";
