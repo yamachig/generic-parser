434a6348db0026ae6e49166acce04621d2c239e4
yamachi <yamachig@users.noreply.github.com>
2022-04-09 21:43:03 -0700

    modified package.json

--- src/pegjs/optionalPegjs.ts
+++ src/pegjs/optionalPegjs.ts
@@ -4,6 +4,7 @@ export const pegjs = (() => {
     try {
         return require("pegjs-dev/packages/pegjs");
     } catch {
+        console.error("pegjs-dev not found. Please install it from https://github.com/pegjs/pegjs/tarball/b7b87ea8aeeaa1caf096e2da99fd95a971890ca1");
         return null;
     }
 })() as (null | typeof peg);
